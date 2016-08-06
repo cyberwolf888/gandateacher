bb9b80f5ca9014f56bc26fff1a4be37b68f843dd
Karen <wijaya.imd@gmail.com>
2016-08-06 18:48:47 +0800

    V.0.5

--- app/src/main/java/com/gandaedukasi/gandateacher/EditProfileActivity.java
+++ app/src/main/java/com/gandaedukasi/gandateacher/EditProfileActivity.java
@@ -4,24 +4,75 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class EditProfileActivity extends AppCompatActivity {
-
+    Button btnSave;
+    TextView teacherTingkatPendidikan, teacherMapel;
+    String name, email, phone, address, pendidikan, tingkat_pendidikan, mapel, zona, photo, id_tingkat_pendidikan, id_mapel;
+    EditText teacherName, editEmail, teacherPhone, teacherEdu, teacherAddress;
+    Spinner Zone;
+    ImageView teacherPhoto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        name = getIntent().getStringExtra("name");
+        email = getIntent().getStringExtra("email");
+        phone = getIntent().getStringExtra("phone");
+        pendidikan = getIntent().getStringExtra("pendidikan");
+        address = getIntent().getStringExtra("alamat");
+        tingkat_pendidikan = getIntent().getStringExtra("label_tingkat_pendidikan");
+        zona = getIntent().getStringExtra("zona");
+        photo = getIntent().getStringExtra("photo");
+        id_tingkat_pendidikan = getIntent().getStringExtra("id_tingkat_pendidikan");
+        id_mapel = getIntent().getStringExtra("id_mapel");
+        mapel = getIntent().getStringExtra("label_mapel");
+
         setContentView(R.layout.activity_edit_profile);
 
-        ImageView btnSave;
+        teacherName = (EditText) findViewById(R.id.teacherName);
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        teacherPhone = (EditText) findViewById(R.id.teacherPhone);
+        teacherEdu = (EditText) findViewById(R.id.teacherEdu);
+        teacherAddress = (EditText) findViewById(R.id.teacherAddress);
 
-        btnSave = (ImageView)findViewById(R.id.btnSave);
+        teacherTingkatPendidikan = (TextView) findViewById(R.id.teacherTingkatPendidikan);
+        teacherMapel = (TextView) findViewById(R.id.teacherMapel);
 
+        btnSave = (Button) findViewById(R.id.btnSave);
+
+        teacherName.setText(name);
+        editEmail.setText(email);
+        teacherPhone.setText(phone);
+        teacherEdu.setText(pendidikan);
+        teacherAddress.setText(address);
+        teacherTingkatPendidikan.setText(tingkat_pendidikan);
+        teacherMapel.setText(mapel);
+
+        teacherTingkatPendidikan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "teacher tingkat pendidikan", Toast.LENGTH_LONG).show();
+            }
+        });
+        teacherMapel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "teacher mapel", Toast.LENGTH_LONG).show();
+            }
+        });
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(EditProfileActivity.this, ProfileActivity.class);
                 startActivity(i);
+                finish();
             }
         });
     }
--- app/src/main/java/com/gandaedukasi/gandateacher/ProfileActivity.java
+++ app/src/main/java/com/gandaedukasi/gandateacher/ProfileActivity.java
@@ -1,18 +1,51 @@
 package com.gandaedukasi.gandateacher;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.gandaedukasi.gandateacher.utility.RequestServer;
+import com.gandaedukasi.gandateacher.utility.Session;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    ProgressDialog pDialog;
+    Session session;
+    TextView teacherName,teacherEmail,teacherPhone,teacherEdukasi,teacherAddress,teacherTingkatPendidikan,teacherMapel,teacherZona;
+    ImageView teacherPhoto;
+    String id_tingkat_pendidikan, id_mapel, photo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        session = new Session(ProfileActivity.this);
         setContentView(R.layout.activity_profile);
+
+        teacherName = (TextView) findViewById(R.id.teacherName);
+        teacherEmail = (TextView) findViewById(R.id.teacherEmail);
+        teacherPhone = (TextView) findViewById(R.id.teacherPhone);
+        teacherEdukasi = (TextView) findViewById(R.id.teacherEdukasi);
+        teacherAddress = (TextView) findViewById(R.id.teacherAddress);
+        teacherTingkatPendidikan = (TextView) findViewById(R.id.teacherTingkatPendidikan);
+        teacherMapel = (TextView) findViewById(R.id.teacherMapel);
+        teacherZona = (TextView) findViewById(R.id.teacherZona);
+
+        teacherPhoto = (ImageView) findViewById(R.id.teacherPhoto);
 
         ImageView btnTry;
 
@@ -21,9 +54,96 @@ public class ProfileActivity extends AppCompatActivity {
         btnTry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = teacherName.getText().toString();
+                String email = teacherEmail.getText().toString();
+                String phone = teacherPhone.getText().toString();
+                String pendidikan = teacherEdukasi.getText().toString();
+                String alamat = teacherAddress.getText().toString();
+                String label_tingkat_pendidikan = teacherTingkatPendidikan.getText().toString();
+                String label_mapel = teacherMapel.getText().toString();
+
                 Intent i = new Intent(ProfileActivity.this, EditProfileActivity.class);
+                i.putExtra("name", name);
+                i.putExtra("email", email);
+                i.putExtra("phone", phone);
+                i.putExtra("pendidikan", pendidikan);
+                i.putExtra("alamat", alamat);
+                i.putExtra("label_tingkat_pendidikan", label_tingkat_pendidikan);
+                i.putExtra("label_mapel", label_mapel);
+                i.putExtra("id_tingkat_pendidikan", id_tingkat_pendidikan);
+                i.putExtra("id_mapel", id_mapel);
+                i.putExtra("photo", photo);
                 startActivity(i);
             }
         });
     }
+
+    public void onResume(){
+        super.onResume();
+        if(isNetworkAvailable()){
+            pDialog = new ProgressDialog(ProfileActivity.this);
+            pDialog.setMessage("Loading...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+            String url = new RequestServer().getServer_url()+"getProfile/pengajar";
+            Log.d("Test Url",">"+url);
+
+            JsonObject jsonReq = new JsonObject();
+            jsonReq.addProperty("user_id", session.getUserId());
+
+            Ion.with(ProfileActivity.this)
+                    .load(url)
+                    //.setLogging("ION_VERBOSE_LOGGING", Log.VERBOSE)
+                    .setJsonObjectBody(jsonReq)
+                    .asJsonObject()
+                    .setCallback(new FutureCallback<JsonObject>() {
+                        @Override
+                        public void onCompleted(Exception e, JsonObject result) {
+                            try{
+                                String status = result.get("status").toString();
+                                if (status.equals("1")){
+                                    JsonObject data = result.getAsJsonObject("data");
+                                    teacherName.setText(data.get("fullname").getAsString());
+                                    teacherEmail.setText(data.get("email").getAsString());
+                                    teacherPhone.setText(data.get("pengajar_cp").getAsString());
+                                    teacherEdukasi.setText(data.get("pengajar_pendidikan").getAsString());
+                                    teacherAddress.setText(data.get("pengajar_alamat").getAsString());
+                                    teacherTingkatPendidikan.setText(data.get("label_tingkat_pendidikan").getAsString());
+                                    teacherMapel.setText(data.get("label_mapel").getAsString());
+                                    teacherZona.setText(data.get("zona").getAsString());
+                                    id_tingkat_pendidikan = data.get("id_tingkat_pendidikan").getAsString();
+                                    id_mapel = data.get("id_mapel").getAsString();
+                                    if(data.get("photo").isJsonNull()){
+                                        photo = "";
+                                    }else{
+                                        photo = data.get("photo").getAsString();
+                                    }
+                                    Ion.with(teacherPhoto)
+                                            .load("http://gandaedukasi.esy.es/images/article/image1.png");
+                                }else{
+                                    //TODO jika status 0
+                                }
+                            }catch (Exception ex){
+                                Toast.makeText(getApplicationContext(), getString(R.string.id_error_network), Toast.LENGTH_LONG).show();
+                            }
+                            pDialog.dismiss();
+                        }
+                    });
+        }else{
+            Toast.makeText(getApplicationContext(), getString(R.string.id_error_network), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public boolean isNetworkAvailable() {
+        ConnectivityManager cm = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+
 }
